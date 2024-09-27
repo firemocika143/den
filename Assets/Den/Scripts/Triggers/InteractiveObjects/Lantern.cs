e38e4fdb3b9ef88a260f49b0cfea4c3d125ff04a
Estel <song105293@gmail.com>
2024-09-27 16:31:58 +0800

    Temporary update

--- Assets/Den/Scripts/Triggers/InteractiveObjects/Lantern.cs
+++ Assets/Den/Scripts/Triggers/InteractiveObjects/Lantern.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using UnityEngine.Rendering.Universal;
 
 public class Lantern : MonoBehaviour
@@ -9,17 +10,38 @@ public class Lantern : MonoBehaviour
     public bool closeEnough = false;
 
     [SerializeField]
-    private Light2D lanternLight2d;
+    // private Light2D lanternLight2d;
+    private GameObject sign;
+
+    private void Start()
+    {
+        sign.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (closeEnough)
+        {
+            /*if (sign.transform.GetChild(0).TryGetComponent<TextMeshPro>(out TextMeshPro text))
+                {
+                    text.text = "E";
+                    text.fontSize = 6;
+                }*/
+        }
+    }
 
     public void IntoDetector()
     {
+        sign.SetActive(true);     
         closeEnough = true;
+        Debug.Log("Triggered (light)");
         //This function should be called by the trigger of the lantern in the inspector
         //TODO - when the player touched the trigger, and if they still have their light on them, show them there is a lantern here
     }
 
     public void OutOfDetector()
     {
+        sign.SetActive(false);  
         closeEnough = false;
         //This function should be called by the trigger of the lantern in the inspector
         //TODO - Stop showing anything to player because they had left the detector
@@ -29,7 +51,7 @@ public class Lantern : MonoBehaviour
     {
         //TODO - play animation & set LightArea collider trigger active(hope this would trigger player OncollisionEnter2d but im not sure)
         //TODO - set the detector game object inactive
-        lanternLight2d.enabled = true;
+        //lanternLight2d.enabled = true;
         lightedOn = true;
     }
 
@@ -37,7 +59,7 @@ public class Lantern : MonoBehaviour
     {
         //TODO - play animation & set LightArea collider trigger inactive
         //TODO - set the detector game object active
-        lanternLight2d.enabled = false;
+        //lanternLight2d.enabled = false;
         lightedOn = false;
     }
 }
