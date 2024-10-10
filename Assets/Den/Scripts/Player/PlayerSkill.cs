8b0ba0323fe28e950d7983df9b5f4a6c6c939686
Furry Penguin <mocika111@gmail.com>
2024-10-10 11:42:42 +0800

    little update

--- Assets/Den/Scripts/Player/PlayerSkill.cs
+++ Assets/Den/Scripts/Player/PlayerSkill.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerSkill : MonoBehaviour
 {
-    public float minDistanceToStartAttack = 1f;
-    public float maxDistanceToStartAttack = 1f;
+    public float minDistanceToStart = 0.1f;
+    public float maxDistanceToStart = 10f;
     public Vector3 previousPosition;
     public bool isNewClick = false;
     public Transform AttackPoint;
@@ -14,34 +14,32 @@ public class PlayerSkill : MonoBehaviour
 
     void Start()
     {
-        AttackPoint.position = new Vector3(AttackPoint.position.x, AttackPoint.position.y, 0);
+        previousPosition = transform.position;
         lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = 1;
     }
 
     private void Update()
     {
-        if(Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0))
         {
-            Draw();
-        }
-    }
+            Vector3 currentPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            currentPosition.z = 0;
+            float distance = Vector3.Distance(previousPosition, currentPosition);
 
-    public void Draw()
-    {
-        if (isNewClick)
-        {
-            previousPosition = AttackPoint.position;
-            isNewClick = false;
-        }
-        Vector3 currentPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        currentPosition.z = 0;
-        float distance = Vector3.Distance(AttackPoint.position, currentPosition);
-
-        if (distance > minDistanceToStartAttack && distance < maxDistanceToStartAttack)
-        {
-            lineRenderer.positionCount++;
-            lineRenderer.SetPosition(lineRenderer.positionCount - 1, currentPosition);
-            previousPosition = currentPosition;
+            if (distance > minDistanceToStart && distance < maxDistanceToStart)
+            {
+                if (previousPosition == transform.position)//this is really a bad way
+                {
+                    lineRenderer.SetPosition(0, currentPosition);
+                }
+                else
+                {
+                    lineRenderer.positionCount++;
+                    lineRenderer.SetPosition(lineRenderer.positionCount - 1, currentPosition);
+                    previousPosition = currentPosition;
+                }
+            }
         }
     }
 }
