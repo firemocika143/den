46d321fe69424552523c03cb64ba25b17fbacee0
Barry1749 <146699243+Barry1749@users.noreply.github.com>
2024-10-15 16:55:24 +0800

    fix PlayerDrawLight2DPosition

--- Assets/Den/Scripts/Player/PlayerDrawLight2DPosition.cs
+++ Assets/Den/Scripts/Player/PlayerDrawLight2DPosition.cs
@@ -13,6 +13,15 @@ public class PlayerDrawLight2DPosition : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // Get the mouse position in screen space
+        Vector3 mousePosition = Input.mousePosition;
+
+        // Convert the screen position to world position
+        // Set the Z value to 0 if you are working in 2D
+        mousePosition.z = Camera.main.nearClipPlane;  // Adjust this value for the correct Z-depth in 3D
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        // Set the object's position to the mouse world position
+        transform.position = new Vector3(worldPosition.x, worldPosition.y, 0);
     }
 }
