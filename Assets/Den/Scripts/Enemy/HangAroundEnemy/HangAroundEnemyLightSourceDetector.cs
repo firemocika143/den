7d5ae9502cf81a214932235e2ccb1b864804f495
Barry1749 <146699243+Barry1749@users.noreply.github.com>
2024-10-17 20:50:56 +0800

    修改Bullet, MainScene, HangAroundEnemyDetector, HangAroundEnemyMovement

--- Assets/Den/Scripts/Enemy/HangAroundEnemy/HangAroundEnemyLightSourceDetector.cs
+++ Assets/Den/Scripts/Enemy/HangAroundEnemy/HangAroundEnemyLightSourceDetector.cs
@@ -5,36 +5,32 @@ using UnityEngine;
 public class HangAroundEnemyLightSourceDetector : MonoBehaviour
 {
     public bool reachLight = false;
-    public int dir = 1;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         //Debug.Log(other.gameObject.name);
-        // if I reach a light source
+        // 
         if (other.CompareTag("LightSource"))
         {
             reachLight = true;
-            dir = -1;
         }
     }
 
     private void OnTriggerStay2D(Collider2D other)
     {
-        // if I stay with player
+        // 
         if (other.CompareTag("LightSource"))
         {
             reachLight = true;
-            dir = 1;
         }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        // if I stay with player
+        // 
         if (other.CompareTag("LightSource"))
         {
             reachLight = false;
-            dir = 1;
         }
     }
 }
--- Assets/Den/Scripts/Enemy/HangAroundEnemy/HangAroundEnemyMovement.cs
+++ Assets/Den/Scripts/Enemy/HangAroundEnemy/HangAroundEnemyMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class HangAroundEnemyMovement : MonoBehaviour
 {
     [SerializeField]
-    private float speed = 3f;
+    private float speed = 5f;
     [SerializeField]
     private Transform groundCheck;
     [SerializeField]
@@ -14,8 +14,8 @@ public class HangAroundEnemyMovement : MonoBehaviour
     public Rigidbody2D rb;
     public GameObject lightSourceDetector;
     private bool reachLight;
-    private int dir;
-
+    private bool escaping;
+    private float escapeTime = 2.0f;
     private float horizontal;
     
     private Vector3 size;
@@ -27,7 +27,7 @@ public class HangAroundEnemyMovement : MonoBehaviour
         Debug.Log(gameObject.name);
         size = GetComponent<Transform>().localScale;
         reachLight = lightSourceDetector.GetComponent<HangAroundEnemyLightSourceDetector>().reachLight;
-        dir = lightSourceDetector.GetComponent<HangAroundEnemyLightSourceDetector>().dir;
+        escaping = false;
 
         float num = Random.Range(0.0f, 1.0f);
         if (num > 0.5f)
@@ -45,40 +45,41 @@ public class HangAroundEnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        reachLight = lightSourceDetector.GetComponent<HangAroundEnemyLightSourceDetector>().reachLight;
-        GroundMovement();
+        if (!escaping)
+        {
+            GroundMovement();
+        }
     }
 
     private void GroundMovement()
     {
-        if(reachLight)
+        if (rb.velocity.x < 0)
         {
-            if (rb.velocity.x < 0)
-            {
-                horizontal = -1;
-            }
-            else
-            {
-                horizontal = 1;
-            }
-            horizontal *= dir;
-            // Moving left or right
-            rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+            horizontal = -1;
         }
         else
         {
-            if (rb.velocity.x < 0)
-            {
-                horizontal = -1;
-            }
+            horizontal = 1;
+        }
+        reachLight = lightSourceDetector.GetComponent<HangAroundEnemyLightSourceDetector>().reachLight;
+        if (reachLight)
+        {
+            horizontal *= -1;
+
+            StartCoroutine(escapeCount());
+
+        }
+        else
+        {
             float num = Random.Range(0.0f, 1.0f);
             if (num > 0.99f)
             {
                 horizontal *= -1;
             }
-            // Moving left or right
-            rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
         }
+
+        // Moving left or right
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
 
         // Flipping
         if (horizontal != 0)
@@ -91,5 +92,14 @@ public class HangAroundEnemyMovement : MonoBehaviour
     private void Flip(float dir)
     {
         transform.localScale = new Vector3(dir * size.x, 1 * size.y, 1);
+    }
+
+    private IEnumerator escapeCount()
+    {
+        escaping = true;
+
+        yield return new WaitForSeconds(escapeTime);
+
+        escaping = false;
     }
 }
