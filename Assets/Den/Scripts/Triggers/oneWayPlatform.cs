a7094cf380b493d7ffcdb804042da7e0a5fabab3
Estel <song105293@gmail.com>
2024-10-19 02:38:49 +0800

    add oneway platform on top of ladders

---
+++ Assets/Den/Scripts/Triggers/oneWayPlatform.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOneWayPlatform : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject currOneWayPlatform;
+    [SerializeField]
+    private BoxCollider2D playerCollider;
+    [SerializeField]
+    private PlayerController playerController;
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (currOneWayPlatform != null)
+            {
+                StartCoroutine(DisableCollision());
+            }
+        }
+    }
+
+    public void CollideWithCollider()
+    {
+        currOneWayPlatform.SetActive(true);
+    }
+
+    public void LeaveCollider()
+    {
+        currOneWayPlatform.SetActive(false);
+    }
+
+    private IEnumerator DisableCollision()
+    {
+        BoxCollider2D platformCollider = currOneWayPlatform.GetComponent<BoxCollider2D>();
+        Physics2D.IgnoreCollision(playerCollider, platformCollider);
+        yield return new WaitForSeconds(0.25f);
+        Physics2D.IgnoreCollision(playerCollider, platformCollider, false);
+        playerController = GetComponent<PlayerController>();
+        playerController.state.climb = true;
+    }
+}
