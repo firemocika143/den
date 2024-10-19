67c6a807bbe7ac9608add5c034ae568fe31f3ed9
Furry Penguin <mocika111@gmail.com>
2024-10-19 19:56:14 +0800

    little update

--- Assets/Den/Scripts/TriggerZone/LightTrigger.cs
+++ Assets/Den/Scripts/TriggerZone/LightTrigger.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
 public class LightTrigger : MonoBehaviour
 {
+    public bool isTriggered = false;
+    public bool isShaking;
+
     [SerializeField]
     private Light2D triggeredLight;
     [SerializeField]
     private float lightOutTime;
+    [SerializeField]
+    private TriggerZone triggerZone;
 
     private float timer;
 
@@ -19,15 +25,35 @@ public class LightTrigger : MonoBehaviour
 
     private void Update()
     {
-        if (triggeredLight.enabled && Time.time - timer >= lightOutTime)
+        if (triggeredLight.enabled && Time.time - timer >= lightOutTime && !triggerZone.solved)
         {
             triggeredLight.enabled = false;
+            isTriggered = false;
         }
     }
 
     public void Triggered()
     {
         triggeredLight.enabled = true;
+        isTriggered = true;
         timer = Time.time;
+    }
+
+    public IEnumerator Shake()
+    {
+        isShaking = true;
+        Vector3 formerPos = transform.parent.position;
+        float amplitude = 0.1f;
+        int sign = -1;
+        float shakeTimer = Time.time;
+        transform.parent.position = new Vector3(formerPos.x - amplitude*sign/2, transform.parent.position.y, 0);
+        while(Time.time - shakeTimer < 0.5f)// I don't think the timer is working correctly
+        {
+            transform.parent.position = new Vector3(formerPos.x + sign * amplitude, transform.parent.position.y, 0);
+            sign *= -1;
+            yield return new WaitForSeconds(0.02f);
+        }
+        transform.parent.position = formerPos;
+        isShaking = false;
     }
 }
--- Assets/Den/Scripts/TriggerZone/TriggerZone.cs
+++ Assets/Den/Scripts/TriggerZone/TriggerZone.cs
@@ -4,7 +4,25 @@ using UnityEngine;
 
 public class TriggerZone : MonoBehaviour
 {
+    public bool solved = false;
     public List<LightTrigger> lightTriggers = new List<LightTrigger>();
+    public bool returning = false;
+
+    private void Update()
+    {
+        if (AllTrigged() && !solved && !returning)
+        {
+            returning = true;
+            foreach (LightTrigger trigger in lightTriggers)
+            {
+                StartCoroutine(trigger.Shake());
+            }
+        }
+        if (AllWaiting() && returning)
+        {
+            returning = false;
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D col)
     {
@@ -21,5 +39,25 @@ public class TriggerZone : MonoBehaviour
         {
             CameraManager.Instance.Follow(col.transform);
         }
+    }
+
+    private bool AllTrigged()
+    {
+        foreach (LightTrigger trigger in lightTriggers)
+        {
+            if (!trigger.isTriggered) return false;
+        }
+
+        return true;
+    }
+
+    private bool AllWaiting()
+    {
+        foreach (LightTrigger trigger in lightTriggers)
+        {
+            if (trigger.isShaking) return false;
+        }
+
+        return true;
     }
 }
