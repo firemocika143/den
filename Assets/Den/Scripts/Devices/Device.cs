3dd951168598a293f0654867592aa468ee010d82
Furry Penguin <mocika111@gmail.com>
2024-11-17 22:21:04 +0800

    update
    [fix] project settings
    [fix] device

--- Assets/Den/Scripts/Devices/Device.cs
+++ Assets/Den/Scripts/Devices/Device.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 using System;
-using UnityEngine.WSA;
 
 public abstract class Device : MonoBehaviour
 {
